a7990722f7e8855a3bab8f12924736c99a252283
Sakan Komolvatin <tutumm96@gmail.com>
2017-10-10 16:02:10 +0700

    Develop Login Page :TUM:

--- App.js
+++ App.js
@@ -1,57 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'Username',
+      password: 'Password'
+   };
+  }
 
-export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcom to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <LinearGradient colors={['#171A2C', '#F0318B']} style={styles.linearGradient}>
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <Text style={styles.logoText}>Walk2Cash</Text>
+          <TextInput style={styles.inputText}
+          onChangeText={(username) => this.setState({username})}
+          placeholder={this.state.username} placeholderTextColor='rgba(255, 255, 255, 0.7)' />
+
+          <TextInput style={styles.inputText}
+          onChangeText={(password) => this.setState({password})}
+          placeholder={this.state.password} placeholderTextColor='rgba(255, 255, 255, 0.7)' />
+
+          <Button style={styles.LoginButton} title="Login" color="#841584" accessibilityLabel="Learn more about this purple button"/>
+          <Button style={styles.LoginButton} title="Sign Up" color="#841584" accessibilityLabel="Learn more about this purple button"/>
+        </View>
+    </LinearGradient>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+var styles = StyleSheet.create({
+  linearGradient: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
   },
-  welcome: {
-    fontSize: 20,
+  logoText: {
+    fontSize: 40,
+    fontFamily: 'Gill Sans',
     textAlign: 'center',
     margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  inputText: {
+    height: 40,
+    width: '80%',
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    color: 'rgba(255, 255, 255, 0.7)',
   },
+  loginButton: {
+    marginTop: 30,
+    borderRadius: 50
+  }
 });
